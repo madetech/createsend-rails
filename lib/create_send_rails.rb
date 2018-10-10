0e3425415034ca7d91608be8992e061b1b361f66
Martin Vandersteen <vandersteenmartin@gmail.com>
2018-10-10 16:47:35 +0200

    0.2.2

--- lib/create_send_rails.rb
+++ lib/create_send_rails.rb
@@ -5,9 +5,7 @@ require 'create_send_rails/ext/hash'
 require 'create_send_rails/smart_email_formatter'
 
 module CreateSendRails
-  module_function :install
-
-  def install
+  def self.install
     ActionMailer::Base.include(CreateSendRails::SmartEmailer)
     ActionMailer::Base.include(CreateSendRails::ViewDefaults)
     ActionMailer::Base.add_delivery_method :create_send, CreateSendRails::Deliverer
