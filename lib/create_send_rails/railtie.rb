8b07227fd9f51fd17190ddb548c9a8dfb581a1d5
Martin Vandersteen <vandersteenmartin@gmail.com>
2018-10-10 17:31:53 +0200

    2.5

--- lib/create_send_rails/railtie.rb
+++ lib/create_send_rails/railtie.rb
@@ -1,6 +1,8 @@
 module CreateSendRails
   class Railtie < Rails::Railtie
+    puts "BLEEEEP"
     initializer 'railtie.createsend-rails' do
+      puts "BLEP"
       CreateSendRails.install
     end
   end
