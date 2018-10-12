71367a5aa9183e841706c528eb122e8202e313cc
Martin Vandersteen <vandersteenmartin@gmail.com>
2018-10-12 10:29:04 +0200

    0.3 working and autoloading now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,4 @@ SimpleCov.start
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'create_send_rails'
+require 'createsend-rails'
