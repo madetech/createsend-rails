e12b0ac0aba91dc56b8684452f1a9d913e8fd5a5
Martin Vandersteen <vandersteenmartin@gmail.com>
2018-10-12 11:26:21 +0200

    remove all derpy puts

--- lib/createsend-rails.rb
+++ lib/createsend-rails.rb
@@ -6,17 +6,14 @@ require 'createsend-rails/smart_email_formatter'
 
 module CreatesendRails
   def self.install
-    puts "INSTALLED"
     ActionMailer::Base.include(CreatesendRails::SmartEmailer)
     ActionMailer::Base.include(CreatesendRails::ViewDefaults)
     ActionMailer::Base.add_delivery_method :create_send, CreatesendRails::Deliverer
   end
 end
-puts "REQUIRED"
+
 if defined?(Rails)
-  puts "RAILS DEFINED"
   require 'createsend-rails/railtie'
 else
-  puts "RAILS UNDEFINED"
   CreatesendRails.install
 end
--- lib/createsend-rails/railtie.rb
+++ lib/createsend-rails/railtie.rb
@@ -1,6 +1,5 @@
 module CreatesendRails
   class Railtie < Rails::Railtie
-    puts "BLEEEEP"
     CreatesendRails.install
   end
 end
